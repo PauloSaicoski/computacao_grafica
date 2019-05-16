a52b3eb81e61abec276033494c1d75c19a069da6
Paulo <pssarkis@inf.ufsm.br>
2019-05-16 12:28:44 -0300

    perna completa falta bicicleta

--- CGT2/src/Bicicleta.h
+++ CGT2/src/Bicicleta.h
@@ -10,29 +10,29 @@
 #include "gl_canvas2d.h"
 #include "Matrizes.h"
 
-
-class figura{
+class figura
+{
 public:
-  std::vector<ponto*> pontos;
+  std::vector<ponto *> pontos;
 
-
-  virtual void desenha(float referenciaX, float referenciaY){}
+  virtual void desenha(float referenciaX, float referenciaY) {}
   virtual void rotaEmEixo(float rad);
-
-
 };
 
-void figura::rotaEmEixo(float rad){
-    matriz m;
+void figura::rotaEmEixo(float rad)
+{
+  matriz m;
 
-    m.MatrizRotacao(rad);
+  m.MatrizRotacao(rad);
 
-    for(int i = 0; i < pontos.size(); i++){
-        pontos[i]->MultiplicadoPorMatriz(m.pontos);
-    }
+  for (int i = 0; i < pontos.size(); i++)
+  {
+    pontos[i]->MultiplicadoPorMatriz(m.pontos);
+  }
 }
 
-class roda:public figura{
+class roda : public figura
+{
 
 public:
   float raioExterno;
@@ -42,273 +42,299 @@ public:
   roda(float raioExterno, float raioInterno, int qntAros);
 
   void desenha(float referenciaX, float referenciaY);
-
 };
 
-roda::roda(float raioExterno, float raioInterno, int qntAros){
+roda::roda(float raioExterno, float raioInterno, int qntAros)
+{
   this->raioExterno = raioExterno;
   this->raioInterno = raioInterno;
-  anguloEntreAros = PI_2/((float)2*qntAros);
+  anguloEntreAros = PI_2 / ((float)2 * qntAros);
   std::cout << anguloEntreAros << std::endl;
-  for (int i = 0; i < qntAros*2; i++){
-    pontos.push_back(new ponto(cos(i*anguloEntreAros),sin(i*anguloEntreAros)));
+  for (int i = 0; i < qntAros * 2; i++)
+  {
+    pontos.push_back(new ponto(cos(i * anguloEntreAros), sin(i * anguloEntreAros)));
   }
 }
 
-
-void roda::desenha(float referenciaX, float referenciaY){
-  for(int i = raioInterno; i<raioExterno; i++){
+void roda::desenha(float referenciaX, float referenciaY)
+{
+  for (int i = raioInterno; i < raioExterno; i++)
+  {
     circle(referenciaX, referenciaY, i, 50);
   }
   matriz m;
-  m.MatrizEscala(raioInterno,raioInterno);
-  m.MatrizTranslacao(referenciaX,referenciaY);
+  m.MatrizEscala(raioInterno, raioInterno);
+  m.MatrizTranslacao(referenciaX, referenciaY);
   ponto p;
-  for(int i = 0; i < pontos.size(); i++){
+  for (int i = 0; i < pontos.size(); i++)
+  {
     p.SetCoordenadas(pontos[i]->x, pontos[i]->y);
     p.MultiplicadoPorMatriz(m.pontos);
     line(referenciaX, referenciaY, p.x, p.y);
   }
-
 }
 
-class quadro:public figura{
+class quadro : public figura
+{
 private:
-    ponto rodaFrontal;
-    ponto rodaTraseira;
-    ponto banco;
-    float escala;
+  ponto rodaFrontal;
+  ponto rodaTraseira;
+  ponto banco;
+  float escala;
 
 public:
-    quadro(float escala);
-    void atualizaRodasBanco(ponto rodaFrontal, ponto rodaTraseira, ponto banco);
-    void desenha(float referenciaX, float referenciaY);
-    ponto retornaRoda(bool frontal);
-    ponto retornaBanco();
+  quadro(float escala);
+  void atualizaRodasBanco(ponto rodaFrontal, ponto rodaTraseira, ponto banco);
+  void desenha(float referenciaX, float referenciaY);
+  ponto retornaRoda(bool frontal);
+  ponto retornaBanco();
 };
 
-quadro::quadro(float escala){
+quadro::quadro(float escala)
+{
 
-    this->escala = escala;
-    pontos.push_back(new ponto(0,0));   //i = 0 -> catraca
-    pontos.push_back(new ponto(-1,0));  //i = 1 -> roda traseira
-    pontos.push_back(new ponto(-0.5,1));//i = 2 -> banco
-    pontos.push_back(new ponto(1,1));   //i = 3 -> ponto quadro abaixo do guidao
-    pontos.push_back(new ponto(1.5,0)); //i = 4 -> roda frontal
-    float y = 1.3;                      //altura desejada para o guidao
-    float x = (3 - y)/2;
-                    //x calculado para guidao permanescer na reta criada entre roda frontal e ultimo ponto do quadro
-    pontos.push_back(new ponto(x, y));  //i = 5 -> guidao
-
-
+  this->escala = escala;
+  pontos.push_back(new ponto(0, 0));    //i = 0 -> catraca
+  pontos.push_back(new ponto(-1, 0));   //i = 1 -> roda traseira
+  pontos.push_back(new ponto(-0.5, 1)); //i = 2 -> banco
+  pontos.push_back(new ponto(1, 1));    //i = 3 -> ponto quadro abaixo do guidao
+  pontos.push_back(new ponto(1.5, 0));  //i = 4 -> roda frontal
+  float y = 1.3;                        //altura desejada para o guidao
+  float x = (3 - y) / 2;
+  //x calculado para guidao permanescer na reta criada entre roda frontal e ultimo ponto do quadro
+  pontos.push_back(new ponto(x, y)); //i = 5 -> guidao
 }
 
-void quadro::atualizaRodasBanco(ponto rodaFrontal, ponto rodaTraseira, ponto banco){
-    this->rodaFrontal = rodaFrontal;
-    this->rodaTraseira = rodaTraseira;
-    this->banco = banco;
+void quadro::atualizaRodasBanco(ponto rodaFrontal, ponto rodaTraseira, ponto banco)
+{
+  this->rodaFrontal = rodaFrontal;
+  this->rodaTraseira = rodaTraseira;
+  this->banco = banco;
 }
 
-void Bezier(ponto p1, ponto p2, ponto p3){
-    float x, y;
-    for(float t = 0; t < 1; t+=0.001){
-        x = pow(1-t, 2)*p1.x + 2*t*(1-t)*p2.x+pow(t,2)*p3.x;
-        y = pow(1-t, 2)*p1.y + 2*t*(1-t)*p2.y+pow(t,2)*p3.y;
-        point(x,y);
-    }
+void Bezier(ponto p1, ponto p2, ponto p3)
+{
+  float x, y;
+  for (float t = 0; t < 1; t += 0.001)
+  {
+    x = pow(1 - t, 2) * p1.x + 2 * t * (1 - t) * p2.x + pow(t, 2) * p3.x;
+    y = pow(1 - t, 2) * p1.y + 2 * t * (1 - t) * p2.y + pow(t, 2) * p3.y;
+    point(x, y);
+  }
 }
-void quadro::desenha(float referenciaX, float referenciaY){
+void quadro::desenha(float referenciaX, float referenciaY)
+{
 
-    matriz m;
-    ponto p[6];
-    m.MatrizEscala(escala, escala);
-    m.MatrizTranslacao(referenciaX, referenciaY);
-    for(int i = 0; i < pontos.size(); i++){
-        p[i].SetCoordenadas(pontos[i]->x, pontos[i]->y);
-        p[i].MultiplicadoPorMatriz(m.pontos);
-        circleFill(p[i].x, p[i].y, 5, 10);
-    }
+  matriz m;
+  ponto p[6];
+  m.MatrizEscala(escala, escala);
+  m.MatrizTranslacao(referenciaX, referenciaY);
+  for (int i = 0; i < pontos.size(); i++)
+  {
+    p[i].SetCoordenadas(pontos[i]->x, pontos[i]->y);
+    p[i].MultiplicadoPorMatriz(m.pontos);
+    circleFill(p[i].x, p[i].y, 5, 10);
+  }
 
-    for(int i = 1; i <=3; i++){ //desenha as linhas que se ligam a catraca
-        line(p[0].x,p[0].y,p[i].x,p[i].y);
-    }
+  for (int i = 1; i <= 3; i++)
+  { //desenha as linhas que se ligam a catraca
+    line(p[0].x, p[0].y, p[i].x, p[i].y);
+  }
 
-    Bezier(p[1],p[2],p[3]); //bezier para desenhar a parte superior do quadro
+  Bezier(p[1], p[2], p[3]); //bezier para desenhar a parte superior do quadro
 
-    line(p[4].x,p[4].y,p[5].x,p[5].y); //linha entre a roda frontal e guidao, passando pelo final do quadro
-    atualizaRodasBanco(p[4], p[1], p[2]);
-
-
-
-}
-
-ponto quadro::retornaRoda(bool frontal){
-    if (frontal){
-        return rodaFrontal;
-    }
-    else{
-        return rodaTraseira;
-    }
+  line(p[4].x, p[4].y, p[5].x, p[5].y); //linha entre a roda frontal e guidao, passando pelo final do quadro
+  atualizaRodasBanco(p[4], p[1], p[2]);
 }
 
-ponto quadro::retornaBanco(){
-    return banco;
+ponto quadro::retornaRoda(bool frontal)
+{
+  if (frontal)
+  {
+    return rodaFrontal;
+  }
+  else
+  {
+    return rodaTraseira;
+  }
 }
 
-class pedal:public figura{
+ponto quadro::retornaBanco()
+{
+  return banco;
+}
+
+class pedal : public figura
+{
 private:
-    ponto fim;
-    float raio;
-    float referenciaX;
-    float referenciaY;
-public:
-    pedal(float raio, float anguloInicial);
-    void desenha(float referenciaX, float referenciaY);
-    ponto retornaFim();
-    ponto retornaFim(float rad);
-    void atualizaFim(ponto fim);
+  ponto fim;
+  float raio;
+  float referenciaX;
+  float referenciaY;
 
+public:
+  pedal(float raio, float anguloInicial);
+  void desenha(float referenciaX, float referenciaY);
+  ponto retornaFim();
+  ponto retornaFim(float rad);
+  void atualizaFim(ponto fim);
 };
 
-pedal::pedal(float raio, float anguloInicial){
-    this->raio = raio;
-    pontos.push_back(new ponto(cos(anguloInicial),sin(anguloInicial)));
+pedal::pedal(float raio, float anguloInicial)
+{
+  this->raio = raio;
+  pontos.push_back(new ponto(cos(anguloInicial), sin(anguloInicial)));
 }
 
-void pedal::atualizaFim(ponto fim){
-    this->fim = fim;
+void pedal::atualizaFim(ponto fim)
+{
+  this->fim = fim;
 }
 
-void pedal::desenha(float referenciaX, float referenciaY){
+void pedal::desenha(float referenciaX, float referenciaY)
+{
 
   this->referenciaX = referenciaX;
   this->referenciaY = referenciaY;
   matriz m;
-  m.MatrizEscala(raio,raio);
-  m.MatrizTranslacao(referenciaX,referenciaY);
+  m.MatrizEscala(raio, raio);
+  m.MatrizTranslacao(referenciaX, referenciaY);
   ponto p;
   p.SetCoordenadas(pontos[0]->x, pontos[0]->y);
   p.MultiplicadoPorMatriz(m.pontos);
   atualizaFim(p);
   line(referenciaX, referenciaY, p.x, p.y);
   circleFill(p.x, p.y, 5, 10);
-
 }
 
-ponto pedal::retornaFim(){
-    return fim;
+ponto pedal::retornaFim()
+{
+  return fim;
 }
 
-ponto pedal::retornaFim(float rad){
-    ponto p(cos(rad),sin(rad));
-    matriz m;
-    m.MatrizEscala(raio,raio);
-    m.MatrizTranslacao(referenciaX,referenciaY);
-    p.MultiplicadoPorMatriz(m.pontos);
-    return p;
-
+ponto pedal::retornaFim(float rad)
+{
+  ponto p(cos(rad), sin(rad));
+  matriz m;
+  m.MatrizEscala(raio, raio);
+  m.MatrizTranslacao(referenciaX, referenciaY);
+  p.MultiplicadoPorMatriz(m.pontos);
+  return p;
 }
 
-class torso:public figura{
+class torso : public figura
+{
 private:
-    float escala;
-public:
-    torso(float escala);
-    void desenha(float referenciaX, float referenciaY);
-    void setMao(ponto guidao);
+  float escala;
 
+public:
+  torso(float escala);
+  void desenha(float referenciaX, float referenciaY);
+  void setMao(ponto guidao);
 };
 
-torso::torso(float escala){
+torso::torso(float escala)
+{
 
-    this->escala = escala;
-    pontos.push_back(new ponto(-0.5,1)); //i = 0 -> cintura
-    pontos.push_back(new ponto(0,2));    //i = 1 -> cabeca
-    float y = 1.5;                       //altura desejada para o ombro
-    float x = (y-2)/2;                   //x calculado para ombro permanescer na reta criada entre cabeca e cintura
-    pontos.push_back(new ponto(x,y));    //i = 2 -> ombro
-    y = 1.3;                             //altura do guidao
-    x = (3 - y)/2;                       //posicao x do guidao
-    pontos.push_back(new ponto(x, y));   //i = 3 -> mao
-    //pontos.push_back(new ponto(0,0));    //i = 4 -> cotovelo
-
+  this->escala = escala;
+  pontos.push_back(new ponto(-0.5, 1)); //i = 0 -> cintura
+  pontos.push_back(new ponto(0, 2));    //i = 1 -> cabeca
+  float y = 1.6;                        //altura desejada para o ombro
+  float x = (y - 2) / 2;                //x calculado para ombro permanescer na reta criada entre cabeca e cintura
+  pontos.push_back(new ponto(x, y));    //i = 2 -> ombro
+  y = 1.3;                              //altura do guidao
+  x = (3 - y) / 2;                      //posicao x do guidao
+  pontos.push_back(new ponto(x, y));    //i = 3 -> mao
+                                        //pontos.push_back(new ponto(0,0));    //i = 4 -> cotovelo
 }
 
-void torso::setMao(ponto guidao){
-    pontos[3]->SetCoordenadas(guidao.x, guidao.y);
+void torso::setMao(ponto guidao)
+{
+  pontos[3]->SetCoordenadas(guidao.x, guidao.y);
 }
 
-void torso::desenha(float referenciaX, float referenciaY){
-    matriz m;
-    m.MatrizEscala(escala,escala);
-    m.MatrizTranslacao(referenciaX,referenciaY);
-    ponto p[4];
-    for(int i = 0; i < pontos.size(); i++){
-        p[i].SetCoordenadas(pontos[i]->x, pontos[i]->y);
-        p[i].MultiplicadoPorMatriz(m.pontos);
-    }
-    color(0,0,0);
-    circleFill(p[1].x, p[1].y, 30, 10);
-    color(0,0,0);
-    line(p[1].x, p[1].y, p[0].x, p[0].y);
-    line(p[2].x, p[2].y, p[3].x, p[3].y);
-
-
+void torso::desenha(float referenciaX, float referenciaY)
+{
+  matriz m;
+  m.MatrizEscala(escala, escala);
+  m.MatrizTranslacao(referenciaX, referenciaY);
+  ponto p[4];
+  for (int i = 0; i < pontos.size(); i++)
+  {
+    p[i].SetCoordenadas(pontos[i]->x, pontos[i]->y);
+    p[i].MultiplicadoPorMatriz(m.pontos);
+  }
+  color(0, 0, 0);
+  circleFill(p[1].x, p[1].y, 30, 10);
+  color(0, 0, 0);
+  line(p[1].x, p[1].y, p[0].x, p[0].y);
+  line(p[2].x, p[2].y, p[3].x, p[3].y);
 }
 
-class perna:public figura{
+class perna : public figura
+{
 private:
-    float tamA, tamB; //tamanhos da primeira e segunda parte da perna
-    ponto banco, pedal;
-public:
-    perna(){}
-    float calcDiff(ponto A, ponto B);
-    float calcAngulo(ponto A, ponto B);
-    void calcTamanhos(float distMax);
-    void SetBancoPedal(ponto banco, ponto pedal);
-    void desenha();
+  float tamA, tamB; //tamanhos da primeira e segunda parte da perna
+  ponto banco, pedal;
 
+public:
+  perna() {}
+  float calcDiff(ponto A, ponto B);
+  float calcAngulo(ponto A, ponto B);
+  void calcTamanhos(float distMax);
+  void SetBancoPedal(ponto banco, ponto pedal);
+  void desenha();
 };
 
-float perna::calcDiff(ponto A, ponto B){
-    float dist;
-    dist = sqrt(pow(A.x-B.x,2) + pow(A.y-B.y,2));
-
-    return dist;
+float perna::calcDiff(ponto A, ponto B)
+{
+  float dist;
+  dist = sqrt(pow(A.x - B.x, 2) + pow(A.y - B.y, 2));
+  std::cout << dist << std::endl;
+  return dist;
 }
 
-float perna::calcAngulo(ponto A, ponto B){
-    float rad;
-    rad = atan2(A.x-B.x, A.y-B.y);
-    std::cout << rad << std::endl;
-    return rad;
+float perna::calcAngulo(ponto A, ponto B)
+{
+  float rad;
+  rad = atan2(A.y - B.y, A.x - B.x);
+  std::cout << rad << std::endl;
+  return rad;
 }
 
-void perna::calcTamanhos(float distMax){
-    tamA = distMax*0.55;
-    tamB = distMax*0.5;
+void perna::calcTamanhos(float distMax)
+{
+  tamA = distMax * 0.51;
+  tamB = distMax * 0.51;
 }
 
-void perna::SetBancoPedal(ponto banco, ponto pedal){
-    this->banco = banco;
-    this->pedal = pedal;
+void perna::SetBancoPedal(ponto banco, ponto pedal)
+{
+  this->banco = banco;
+  this->pedal = pedal;
 }
 
-void perna::desenha(){
-    float angBancoPedal;
-    float dist;
-    angBancoPedal = calcAngulo(banco, pedal);
-    dist = calcDiff(banco, pedal);
-    float anguloJoelhoTemporario = acos((pow(tamA,2) + pow(dist,2) - pow(tamB,2))/(2*tamA*dist));
-    float radFinal = angBancoPedal - anguloJoelhoTemporario;
-    ponto p(cos(radFinal),sin(radFinal));
-    matriz m;
-    m.MatrizEscala(tamA, tamA);
-    m.MatrizTranslacao(banco.x, banco.y);
-    p.MultiplicadoPorMatriz(m.pontos);
-    line (banco.x, banco.y, p.x, p.y);
-    line (p.x, p.y, pedal.x, pedal.y);
-    circleFill(p.x, p.y, 3, 10);
+void perna::desenha()
+{
+  float angBancoPedal;
+  float dist;
+  angBancoPedal = calcAngulo(pedal, banco);
+  dist = calcDiff(banco, pedal);
+  float anguloJoelhoTemporario = acos((pow(tamA, 2) + pow(dist, 2) - pow(tamB, 2)) / (2 * tamA * dist));
+  float radFinal = angBancoPedal + anguloJoelhoTemporario;
+  ponto p(cos(radFinal), sin(radFinal));
+  matriz m;
+  m.MatrizEscala(tamA, tamA);
+  m.MatrizTranslacao(banco.x, banco.y);
+  p.MultiplicadoPorMatriz(m.pontos);
+  line(banco.x, banco.y, p.x, p.y);
+  line(p.x, p.y, pedal.x, pedal.y);
+  circleFill(p.x, p.y, 3, 10);
 }
+
+
+class bicicleta:public figura{
+
+};
 
 #endif
--- CGT2/src/main_cpp.cpp
+++ CGT2/src/main_cpp.cpp
@@ -7,7 +7,7 @@
 //  Tem tratamento de mosue
 //  Estude o OpenGL antes de tentar compreender o arquivo gl_canvas.cpp
 //
-//  Instruções:
+//  Instruï¿½ï¿½es:
 //    Para compilar em C, basta comentar o comando #define _CPP_
 //	  Para alterar a animacao, digite numeros entre 1 e 7
 // *********************************************************************/
@@ -16,8 +16,8 @@
   Autor: Cesar Tadeu Pozzer
          04/2013
 
-  Instruções:
-	  Para alterar a animacao, digite numeros entre 1 e 7
+  Instruï¿½ï¿½es:
+     Para alterar a animacao, digite numeros entre 1 e 7
 */
 
 #include <GL/glut.h>
@@ -32,100 +32,102 @@
 //#include "Matrizes.h"
 #include "Bicicleta.h"
 
-//variaveis globais
-int   opcao  = 50;
-float global = 0;
-ponto* teste = nullptr;
-figura* r1 = nullptr;
-figura* r2 = nullptr;
-quadro* q = nullptr;
-pedal* p1 = nullptr;
-pedal* p2 = nullptr;
-figura* t = nullptr;
-perna* pern = nullptr;
+#define LARGURA_TELA 1500
+#define ALTURA_TELA 900
 
+//variaveis globais
+int opcao = 50;
+float global = 0;
+ponto *teste = nullptr;
+figura *r1 = nullptr;
+figura *r2 = nullptr;
+quadro *q = nullptr;
+pedal *p1 = nullptr;
+pedal *p2 = nullptr;
+figura *t = nullptr;
+perna *pern1 = nullptr;
+perna *pern2 = nullptr;
 
 //funcao chamada continuamente. Deve-se controlar o que desenhar por meio de variaveis
 //globais que podem ser setadas pelo metodo keyboard()
 void render()
 {
 
-
   static float angulo = 0;
-  angulo=-0.01;
+  angulo = -0.01;
   color(1, 0.5, 0.5);
-  p1->desenha(750,250);
-  p1->rotaEmEixo(angulo/2);
+  p1->desenha(750, 250);
+  p1->rotaEmEixo(angulo / 2);
   color(0, 0, 0);
   //r->desenha(250, 250);
   r1->rotaEmEixo(angulo);
   r2->rotaEmEixo(angulo);
-  color(1,0,0);
+  color(1, 0, 0);
   q->desenha(750, 250);
-  ponto po, po2;
+  ponto po, po2(750, 250);
   color(0.5, 0.5, 0.5);
   po = q->retornaRoda(false);
   r1->desenha(po.x, po.y);
   po = q->retornaRoda(true);
   r2->desenha(po.x, po.y);
   color(1, 0.5, 0.5);
-  p2->desenha(750,250);
-  p2->rotaEmEixo(angulo/2);
+  p2->desenha(750, 250);
+  p2->rotaEmEixo(angulo / 2);
   color(0, 1, 0);
   t->desenha(750, 250);
   po = q->retornaBanco();
-  po2 = p1->retornaFim(pern->calcAngulo(po2, po));
-  pern->calcTamanhos(pern->calcDiff(po, po2));
+  po2 = p1->retornaFim(pern1->calcAngulo(po2, po));
+  pern1->calcTamanhos(pern1->calcDiff(po, po2));
+  pern2->calcTamanhos(pern2->calcDiff(po, po2));
   po2 = p2->retornaFim();
-  pern->SetBancoPedal(po, po2);
-  pern->desenha();
-
+  pern1->SetBancoPedal(po, po2);
+  pern1->desenha();
 }
 
 //funcao chamada toda vez que uma tecla for pressionada
 void keyboard(int key)
 {
-   printf("\nTecla: %d" , key);
-   if( key < 200 )
-   {
-      opcao = key;
-   }
+  printf("\nTecla: %d", key);
+  if (key < 200)
+  {
+    opcao = key;
+  }
 
-   switch(key)
-   {
-      case 27:
-	     exit(0);
-	  break;
-
-   }
+  switch (key)
+  {
+  case 27:
+    exit(0);
+    break;
+  }
 }
 
 //funcao chamada toda vez que uma tecla for liberada
 void keyboardUp(int key)
 {
-   //printf("\nLiberou: %d" , key);
+  //printf("\nLiberou: %d" , key);
 }
 
 //funcao para tratamento de mouse: cliques, movimentos e arrastos
 void mouse(int button, int state, int wheel, int direction, int x, int y)
 {
-   //printf("\nmouse %d %d %d %d %d %d", button, state, wheel, direction,  x, y);
+  //printf("\nmouse %d %d %d %d %d %d", button, state, wheel, direction,  x, y);
 }
 
 int main(void)
 {
-   initCanvas(1500,1000, "Titulo da Janela: Canvas 2D - Pressione 1, 2, 3, 4 ou 5");
+  initCanvas(LARGURA_TELA, ALTURA_TELA, "Titulo da Janela: Canvas 2D - Pressione 1, 2, 3, 4 ou 5");
 
-   r1 = new roda(85, 75, 4);
-   r2 = new roda(85, 75, 4);
-   q = new quadro(100);
-   p1 = new pedal(30, 0);
-   p2 = new pedal(30, PI_2*0.5);
-   t = new torso(100);
-   pern = new perna();
+  r1 = new roda(85, 75, 4);
+  r2 = new roda(85, 75, 4);
+  q = new quadro(100);
+  p1 = new pedal(30, 0);
+  p2 = new pedal(30, PI_2 * 0.5);
+  t = new torso(100);
+  pern1 = new perna();
+  pern2 = new perna();
 
-   //b = new Bola();
-   //r = new Relogio();
+  //b = new Bola();
+  //r = new Relogio();
 
-   runCanvas();
+  runCanvas();
 }
